110a1374b3d9873020bc75192ca12b2c40285e57
OnurBasci <68737523+OnurBasci@users.noreply.github.com>
2023-11-27 16:26:15 +0100

    Glouton Fas

--- graph.py
+++ graph.py
@@ -1,4 +1,5 @@
 import numpy as np 
+from copy import deepcopy
 
 class Graph:
     def __init__(self, liste_sommets):
@@ -40,6 +41,57 @@ class Graph:
                 for neighbor, weight in self.graph[vertex]:
                     self.distances[i][vertex] = min(self.distances[i][vertex], self.distances[i-1][neighbor] + weight)
         return self.distances
+
+    def get_sources(self):
+        sources = []
+        for vertex in self.graph.keys():
+            if len(self.graph[vertex] > 0 and len(self.get_precedent(vertex)) <= 0):
+                sources.append(vertex)
+
+        return sources
+
+    def get_puits(self):
+        puits = []
+        for vertex in self.graph.keys():
+            if len(self.graph[vertex] <= 0 and len(self.get_precedent(vertex)) > 0):
+                puits.append(vertex)
+
+        return puits
+
+    def delete_vertex(self, vertex_to_delete):
+        #remove vertex
+        del self.graph[vertex_to_delete]
+        #remove precedents
+        for vertex in self.graph.keys():
+            for arcs in self.graph[vertex]:
+                if arcs[0] == vertex_to_delete:
+                    self.graph[vertex].remove(arcs)
+
+    def get_diff_enter_exit(self, vertex, graph):
+        sum_enter = sum(precedent[1] for precedent in graph.get_precedent(vertex))
+        sum_exit = sum(neighbor[1] for neighbor in graph[vertex])
+        return sum_exit - sum_enter
+
+    def GloutonFas(self):
+        s1 = []
+        s2 = []
+
+        graphe = deepcopy(self.graph)
+        while len(graphe.keys()) > 0:
+            while len(self.get_sources()) > 0:
+                u = self.get_sources()[0]
+                s1.insert(u, 0)
+                graphe.delete_vertex(u)
+            while len(self.get_puits()) > 0:
+                u = self.get_puits()[0]
+                s2.append(u)
+                graphe.delete_vertex(u)
+
+            u_max = np.argmax(np.array([self.get_diff_enter_exit(vertex, graphe) for vertex in graphe.keys()]))
+            s1.append(u_max)
+            graphe.delete_vertex(u_max)
+
+        return s1.extend(s2)
 
 
 
