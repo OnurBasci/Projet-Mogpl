c2f325ec3bb444492d316bb3be980ec12ed436e6
Tikai7 <halimi.abdelkrim@outlook.fr>
2023-11-27 22:14:41 +0100

    version 1.5

--- graph.py
+++ graph.py
@@ -2,18 +2,25 @@ import numpy as np
 from copy import deepcopy
 
 class Graph:
-    def __init__(self, liste_vertex):
+    def __init__(self, vertex_order):
         # liste des sommets
-        self.liste_vertex = liste_vertex
+        self.vertex_order = vertex_order
         # représentation du graph sous forme de listes d'adjacence
-        self.graph = {x : [] for x in self.liste_vertex}
+        self.graph = {x : [] for x in self.vertex_order}
         # distances pour l'algorithm de Bellman-Ford
         self.distances = None
         # prédecesseurs pour l'algorithm de Bellman-Ford
         self.path = None
+        # nombre d'itérations pour l'algorithm de Bellman-Ford
+        self.nb_iter = 0
+        
+    # ajoute une liste d'arcs au graph
+    def add_edges(self, liste_arcs):
+        for u,v,w in liste_arcs:
+            self.add_edge_weight(u,v,w)
 
     # ajoute un arc entre u et v avec un poids w
-    def add_edge_weight(self, u, v,w):
+    def add_edge_weight(self, u, v, w):
         self.graph[u].append((v, w))
 
     # retourne la liste des prédécesseurs de vertex
@@ -27,16 +34,16 @@ class Graph:
 
     # calcule le plus court chemin entre vertex_start et vertex_end avec l'algorithme de Bellman-Ford
     def compute_bellmanford(self, vertex_start, vertex_end):
-        nb_iter = 0
-        nb_vertices = len(self.liste_vertex)
+        self.nb_iter = 0
+        nb_vertices = len(self.vertex_order)
 
         self.distances = np.zeros((nb_vertices-1,nb_vertices))
         self.distances.fill(np.inf)
         self.distances[:,vertex_start] = 0
         self.predecessors = np.zeros(self.distances.shape)-1
 
-        for i in range(1, len(self.liste_vertex)-1):
-            for vertex in self.liste_vertex:
+        for i in range(1, len(self.vertex_order)-1):
+            for vertex in self.vertex_order:
                 if self.distances[i-1,vertex] == np.inf:
                     continue
                 for neighbor, weight in self.graph[vertex]:
@@ -45,15 +52,15 @@ class Graph:
                         self.distances[i,neighbor] = new_distance
                         self.predecessors[i, neighbor] = vertex
 
-            if self.distances[i,vertex_end] == np.inf:
-                nb_iter += 1 
+            self.nb_iter += 1
+            if self.distances[i,:].all() == self.distances[i-1,:].all():
+                break
 
         self.path = self.reconstruct_path(vertex_start,vertex_end)
-        return self.path,self.distances,nb_iter
+        return self.path,self.distances,self.nb_iter
     
     def reconstruct_path(self, vertex_start,vertex_end):
         path = [vertex_end]
-        print(self.predecessors)
         while vertex_end != -1 and vertex_end != vertex_start:
             vertex_end = int(self.predecessors[-1,vertex_end])
             if vertex_end != -1:
@@ -62,6 +69,14 @@ class Graph:
             path.append(vertex_start)
         return path[::-1]
     
+    def show_bellmanford_info(self):
+        print("nb_iter:")
+        print(self.nb_iter)
+        print("path:")
+        print(self.path)
+        print("distances:")
+        print(self.distances)
+
     def get_path_distance(self):
         return self.path,self.distances
 
